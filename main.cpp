8ea5d5ecf5e06ff120c967861cb4ff02cdd3f776
Klaudia L <klaudia.lalova@gmail.com>
2026-04-06 23:48:53 +0200

    check N - E

--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@
 
 using namespace std;
 
-int main() {
-    Graph g;
-    ifstream G_source("TestData/SmallGraph1.txt");
-
+int GetInput(Graph* g, string source) {
+    ifstream G_source(source);
     if (!G_source.is_open()) {
         cerr << "Error opening the file!";
+        G_source.close();
         return 1;
     }
     string a;
@@ -20,18 +19,35 @@ int main() {
 
     while (getline(G_source, a, ' ')) {
         getline(G_source, b);
-        g.CreateEdge(stoi(a), stoi(b));
+        g->CreateEdge(stoi(a), stoi(b));
     }
-    
+
     G_source.close();
+    return 0;
+}
+
+int main() {
+    Graph g;
+    string s = "TestData/MediumGraph2.txt";
+
+    if (GetInput(&g, s) == 1) {
+        return 1;
+    }
 
     cout << g.GetEdges() << endl;
     cout << g.GetNodes() << endl;
+
+    if (g.GetNodes() - g.GetEdges() != 1) {
+        cout << "not a tree" << endl;
+        return 0;
+    }
 
     auto v = g.Neighbors(3);
 
     for (auto i : v)
         cout << i << " ";
 
+    //odoberanie "listov"
+    
     return 0;
 }
