9b423944c9919d2f359a311adb49a86357efaf1a
Klaudia L <klaudia.lalova@gmail.com>
2026-04-06 23:19:16 +0200

    Test data for my assingment, get input from .txt and can create a graph from it

--- main.cpp
+++ main.cpp
@@ -2,23 +2,36 @@
 #include <map>
 #include <vector>
 #include <fstream>
+#include <string>
 #include "Graph.h"
 
 using namespace std;
 
 int main() {
     Graph g;
-    g.CreateEdge(4, 1);
-    g.CreateEdge(4, 2);
-    g.CreateEdge(3, 4);
-    g.CreateEdge(5, 4);
-    g.CreateEdge(6, 5);
+    ifstream G_source("TestData/SmallGraph1.txt");
+
+    if (!G_source.is_open()) {
+        cerr << "Error opening the file!";
+        return 1;
+    }
+    string a;
+    string b;
+
+    while (getline(G_source, a, ' ')) {
+        getline(G_source, b);
+        g.CreateEdge(stoi(a), stoi(b));
+    }
+    
+    G_source.close();
 
     cout << g.GetEdges() << endl;
     cout << g.GetNodes() << endl;
 
-    auto v = g.Neighbors(4);
+    auto v = g.Neighbors(3);
 
     for (auto i : v)
         cout << i << " ";
+
+    return 0;
 }
